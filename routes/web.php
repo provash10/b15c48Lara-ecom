f7a1f016eb694e3587f00b26405fa7891c434ae7
provash10 <provash20cb@gmail.com>
2024-12-26 23:33:52 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-// http request (url) =>>> 
-Route::get('/',[HomeController::class,'index']);
-Route::get('/contact-us',[HomeController::class,'contact']);
+Route::get('/',[FrontendController::class,'index']);
+Route::get('/contact-us',[FrontendController::class,'contact']);
